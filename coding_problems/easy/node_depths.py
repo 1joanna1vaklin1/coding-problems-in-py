81de1cf8e530bb49519c207281038f2acb617456
joannav <vaklinjoanna@gmail.com>
2024-01-07 09:44:54 -0600

    node depth recursive clean approach.

--- coding_problems/easy/node_depths.py
+++ coding_problems/easy/node_depths.py
@@ -23,6 +23,14 @@ class BinaryTree:
         self.left = None
         self.right = None
 
+# time 0(n) and space O(h) height of the tree.
+# much cleaner approach to the recursive algorithm. + space complexity gets
+# reduced from the above algorithm. from 0(n) to o(h)
+def nodeDepths_recursive(root, depth=0):
+    if root is None:
+        return 0
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+
 
 if __name__ == '__main__':
     nodeDepths()
